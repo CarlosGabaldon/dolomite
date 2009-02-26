35552c427f9bbc997c3747fc8627eba852c28c87
Christian Carey <ccarey@apophxf130dh1.(none)>
2009-02-26 10:54:59 +0800

    correct a few things after running a test of the create script.
    Signed-off-by: Carlos Gabaldon <cgabaldon@cox.net>

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -5,7 +5,10 @@ easy_install web.py
 easy_install python-memcached
 deactivate
 
-wget -c http://cloud.github.com/downloads/cccarey/dolomite/tools-ubuntu-8.10.tgz
+wget -c http://github.com/cccarey/dolomite/raw/master/tools-ubuntu-8.10.tgz
 
 tar xvf tools-ubuntu-8.10.tgz
+
+rm tools-ubuntu-8.10.tgz
+
 
